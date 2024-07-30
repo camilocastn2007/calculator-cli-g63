281b22a069797e20fd90175920c418fa4386dedf
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2024-07-30 11:41:07 -0500

    creat add method subtrack and divide methods

---
+++ calculator/cli.py
@@ -0,0 +1,8 @@
+import click
+
+from calculator.model import Calculator
+
+@click.group()
+@click.pass_context
+def calc(ctx: click.context):
+
--- calculator/model.py
+++ calculator/model.py
@@ -1,4 +1,17 @@
 class Calculator:
 
-    def add(self, a: float ,b:float )->float:
+    def add(self, a: float ,b:float )-> float:
         return a + b
+
+    def subtract(self,a: float ,b: float)-> float:
+        return a - b
+
+    def multiply(self, a: float ,b: float)-> float:
+        return a * b
+
+    def divide(self,a:float,b:float )-> float:
+      if b== 0;
+        return "cannot divide by 0 =>:x"
+
+    return a /  b
+
